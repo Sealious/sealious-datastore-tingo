45f876f2908eb493d738b8e012d1c20c2946d943
modliszka <nikapia@wp.pl>
2015-09-02 14:22:39 +0200

    improve typos

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
 var Promise = require("bluebird");
 var Sealious = require("sealious");
-var path = require("path");
-var fs = require("fs");
+var Path = require("path");
+var Fs = require("fs");
 var TingoDB = require('tingodb')();
 
 var db = null;
 
-var datastore_mongo = new Sealious.ChipTypes.Datastore("mongo");
-
-Sealious.ConfigManager.set_default_config(
-	"datastore_mongo", 
-	{
-		embedded: false,
-		host: 'localhost',
-		port: 27017,
-		db_name: 'sealious'
-	}
-);
-
 var DatastoreTingo = new function(){
 	this.start = function(){
-		var storage_path = path.resolve("./db");
+		var storage_path = Path.resolve("./db");
 
 		try{
-			var stats = fs.lstatSync(storage_path);			
+			var stats = Fs.lstatSync(storage_path);			
 		}catch(error){
 			//the dir does not exist
-			fs.mkdirSync(storage_path);			
+			Fs.mkdirSync(storage_path);			
 		}
 
 		db = new TingoDB.Db('./db', {});
