3c2fdc81bb5b9fd7ee9df96d0babf84cead4c7ab
modliszka <nikapia@wp.pl>
2015-09-03 15:08:39 +0200

    Add connection to Sealious.ChipTypes

--- index.js
+++ index.js
@@ -3,25 +3,31 @@ var Sealious = require("sealious");
 var Path = require("path");
 var Fs = require("fs");
 var TingoDB = require('tingodb')();
+var DbsCommonPart = require('sealious-datastore-dbs-common-part');
 
 var db = null;
 
-var DatastoreTingo = new function(){
-	this.start = function(){
-		var storage_path = Path.resolve("./db");
+var DatastoreTingo = new Sealious.ChipTypes.Datastore("tingodb");
 
-		try{
-			var stats = Fs.lstatSync(storage_path);			
-		}catch(error){
-			//the dir does not exist
-			Fs.mkdirSync(storage_path);			
-		}
+Sealious.ConfigManager.set_config("tingodb_datastore", {
+	"storage_dir": "./db"
+});
 
-		db = new TingoDB.Db('./db', {});
-		console.log("Statring datastore tingo");
-		return Promise.resolve();
+DatastoreTingo.start = function(){
+	var storage_path = Path.resolve("./db");
+
+	try{
+		var stats = Fs.lstatSync(storage_path);			
+	}catch(error){
+		//the dir does not exist
+		Fs.mkdirSync(storage_path);			
 	}
-}
+
+	db = new TingoDB.Db('./db', {});
+	//console.log("Statring datastore tingo");
+	return Promise.resolve();
+};
+
 
 module.exports = DatastoreTingo;
 
