843eeefddefb5a14078110175b496566cfb6405d
Kuba Orlik <kontakt@jakub-orlik.pl>
2016-11-01 21:27:15 +0100

    Reflect changes in sealcode/sealious#06221fe752097f2d2

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 var Promise = require("bluebird");
-var Sealious = require("sealious");
 var Path = require("path");
 var Fs = require("fs");
 var TingoDB = require('tingodb')();
 var DbsCommonPart = require('sealious-datastore-dbs-common-part');
 
-var private = {db: null};
+module.exports = function(app){
+	const DatastoreTingo = app.createDatastore({name:"tingo"});
 
-var DatastoreTingo = new Sealious.ChipTypes.Datastore("tingodb");
+	var private = {db: null};
 
-DatastoreTingo.start = function(){
-	var storage_path = Path.resolve("./db");
+	DatastoreTingo.start = function(){
+		var storage_path = Path.resolve("./db");
 
-	try{
-		var stats = Fs.lstatSync(storage_path);
-	}catch(error){
-		//the dir does not exist
-		Fs.mkdirSync(storage_path);
-	}
+		try{
+			var stats = Fs.lstatSync(storage_path);
+		}catch(error){
+			//the dir does not exist
+			Fs.mkdirSync(storage_path);
+		}
 
-	private.db = new TingoDB.Db('./db', {});
+		private.db = new TingoDB.Db('./db', {});
 
-	return this.post_start();
+		return this.post_start();
+	};
+
+	DbsCommonPart(app, DatastoreTingo,private);
 };
-
-DatastoreTingo = DbsCommonPart(DatastoreTingo,private);
-
-module.exports = DatastoreTingo;
